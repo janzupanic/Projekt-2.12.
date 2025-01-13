df978cec5b40c8ddc33e8e971d9c1ace54f49672
janzupanic <janzupanic7@gmail.com>
2025-01-13 09:30:31 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,57 @@
+import { Router, Route, A } from "@solidjs/router";
+import { AuthProvider, useAuth } from "./components/AuthProvider";
+import { Show } from "solid-js";
+
+import Home from "./pages/Home";
+import SignIn from "./pages/SignIn";
+import Signout from "./pages/Signout";
+
+export default function App() {
+  return (
+    <AuthProvider>
+  
+    <Router root={Layout}>
+      <Route path="/" component={Home} />
+      <Route path="/signin" component={SignIn} />
+      <Route path="/signout" component={Signout} />
+    </Router>
+    </AuthProvider>
+  );
+}
+
+function Layout(props) {
+  const appName = import.meta.env.VITE_APP_NAME;
+  const session = useAuth();
+
+  return (
+    <div class="p-4 flex-col gap-4">
+      
+      <div>
+        <div class="text-4xl text-neutral-500 uppercase">
+          {appName}
+          </div>
+
+          <div class="flex gap-2">
+            <A href="/" class="bg-orange-400 p-2 rounded hover;bg-red-300">Naslovnica</A>
+
+            <Show when={session()}>
+            <A href="/signout" class="bg-orange-400 p-2 rounded hover;bg-red-300">Odjava</A>
+            </Show>
+            
+            <Show when={!session()}>
+            <A href="/signin" class="bg-orange-400 p-2 rounded hover;bg-red-300">Prijava</A>
+            </Show>
+            
+            
+          </div>
+        </div>
+
+<div class="min-h-[75vh] w-10/12 mx-auto">
+    {props.children}
+    </div>
+    <div class="text-center text-xs">
+      Sva prava podržana {new Date().getFullYear()}. Pero i sinovi
+      </div>
+    </div>
+  );
+}
